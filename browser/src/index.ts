3c310418372227e7b10ec27c9ad77afd6aafaf48
Dominik Sumer <domi.sumer@gmail.com>
2024-01-02 20:48:30 +0100

    implemented user id generation with cookies

--- browser/src/index.ts
+++ browser/src/index.ts
@@ -1,6 +1,17 @@
+import { customAlphabet } from 'nanoid';
+
+const alphabet = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+const customId = customAlphabet(alphabet, 21);
+
 type Options = {
   url?: string;
 };
+
+const generateUserId = () => {
+  return customId();
+};
+
+const COOKIE_NAME = '_vuid';
 
 const DEFAULT_OPTIONS: Options = {
   url: 'https://hub.vemetric.com',
@@ -11,6 +22,20 @@ class Vemetric {
 
   init(options?: Options) {
     this.options = { ...DEFAULT_OPTIONS, ...options };
+
+    const cookieNames = document.cookie.split(/; */).map(function (cookie) {
+      const keyValue = cookie.split('=');
+      return keyValue[0];
+    });
+
+    const hasUserIdCookie = cookieNames.some((cookieName) => cookieName === COOKIE_NAME);
+    if (!hasUserIdCookie) {
+      const userId = generateUserId();
+      const domain = window.location.hostname.split('.').slice(-2).join('.');
+      const expiresAt = new Date();
+      expiresAt.setTime(expiresAt.getTime() + 63072e6);
+      document.cookie = `${COOKIE_NAME}=${userId};path=/;SameSite=Lax;expires=${expiresAt.toUTCString()};domain=${domain}`;
+    }
   }
 
   track() {
@@ -20,6 +45,7 @@ class Vemetric {
 
     const req = new XMLHttpRequest();
     req.open('POST', `${this.options.url}/e`, true);
+    req.withCredentials = true;
     req.setRequestHeader('Content-Type', 'application/json');
     req.send(JSON.stringify(payload));
   }
