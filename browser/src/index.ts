86f52d5924da2ca43b9fb48e31e362b96dcc3516
Dominik Sumer <domi.sumer@gmail.com>
2024-01-14 09:48:01 +0100

    distinguish between pageviews and other events

--- browser/src/index.ts
+++ browser/src/index.ts
@@ -6,41 +6,92 @@ const DEFAULT_OPTIONS: Options = {
   url: 'https://hub.vemetric.com',
 };
 
+function getBasicEventData() {
+  return {
+    url: window.location.href,
+  };
+}
+
+function getBasicEventHeaders() {
+  let referrer = document.referrer || undefined;
+  if (referrer === window.location.href) {
+    referrer = undefined;
+  }
+
+  return {
+    'v-referrer': referrer || undefined,
+  };
+}
+
 class Vemetric {
   private options: Options = DEFAULT_OPTIONS;
+  private isInitialized = false;
   private identifier?: string;
 
   init(options?: Options) {
     this.options = { ...DEFAULT_OPTIONS, ...options };
+    this.isInitialized = true;
   }
 
-  private sendRequest(path: string, payload?: object) {
+  private checkInitialized() {
+    if (!this.isInitialized) {
+      throw new Error('Vemetric is not initialized yet.');
+    }
+  }
+
+  private sendRequest(path: string, payload?: Record<string, unknown>, headers?: Record<string, string | undefined>) {
     const req = new XMLHttpRequest();
     req.open('POST', `${this.options.url}${path}`, true);
     req.withCredentials = true;
     req.setRequestHeader('Content-Type', 'application/json');
+    if (headers) {
+      Object.entries(headers).forEach(([key, value]) => {
+        if (!value) {
+          return;
+        }
+
+        req.setRequestHeader(key, value);
+      });
+    }
     req.send(payload ? JSON.stringify(payload) : undefined);
   }
 
-  track() {
-    // TODO: check if init has been called
+  trackPageView() {
+    this.checkInitialized();
 
     const payload = {
-      o: window.location.href,
+      ...getBasicEventData(),
     };
 
-    this.sendRequest('/e', payload);
+    const headers = getBasicEventHeaders();
+
+    this.sendRequest('/p', payload, headers);
+  }
+
+  trackEvent(name: string, customData?: Record<string, unknown>) {
+    this.checkInitialized();
+
+    const payload = {
+      ...getBasicEventData(),
+      name,
+      customData,
+    };
+
+    const headers = getBasicEventHeaders();
+
+    this.sendRequest('/e', payload, headers);
   }
 
   resetUser() {
-    // TODO: check if init has been called
+    this.checkInitialized();
     this.identifier = undefined;
 
+    // TODO: implement on backend side
     this.sendRequest('/r');
   }
 
   identify(identifier: string) {
-    // TODO: check if init has been called
+    this.checkInitialized();
 
     if (this.identifier === identifier) {
       return;
