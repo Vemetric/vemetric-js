042ef7b6ae446f788a0f61a3d279df442d000628
Dominik Sumer <domi.sumer@gmail.com>
2024-03-03 22:20:46 +0100

    event autotracking of pageviews, peageleaves and outbound links

--- browser/src/index.ts
+++ browser/src/index.ts
@@ -1,20 +1,28 @@
 type Options = {
   url?: string;
+  trackPageViews?: boolean;
+  trackOutboundLinks?: boolean;
 };
 
 const DEFAULT_OPTIONS: Options = {
   url: 'https://hub.vemetric.com',
+  trackPageViews: true,
+  trackOutboundLinks: true,
 };
+
+function getCurrentUrl() {
+  return window.location.href;
+}
 
 function getBasicEventData() {
   return {
-    url: window.location.href,
+    url: getCurrentUrl(),
   };
 }
 
 function getBasicEventHeaders() {
   let referrer = document.referrer || undefined;
-  if (referrer === window.location.href) {
+  if (referrer === getCurrentUrl()) {
     referrer = undefined;
   }
 
@@ -27,10 +35,27 @@ class Vemetric {
   private options: Options = DEFAULT_OPTIONS;
   private isInitialized = false;
   private identifier?: string;
+  private lastViewedPage?: string;
 
   init(options?: Options) {
+    if (this.isInitialized) {
+      return;
+    }
+
     this.options = { ...DEFAULT_OPTIONS, ...options };
     this.isInitialized = true;
+
+    window.addEventListener('beforeunload', () => {
+      this.trackEvent('$$pageLeave', undefined, true);
+    });
+
+    if (this.options.trackPageViews) {
+      this.trackPageView();
+      this.enableTrackPageViews();
+    }
+    if (this.options.trackOutboundLinks) {
+      this.enableTrackOutboundLinks();
+    }
   }
 
   private checkInitialized() {
@@ -59,34 +84,44 @@ class Vemetric {
   trackPageView() {
     this.checkInitialized();
 
-    const payload = {
-      ...getBasicEventData(),
-    };
+    const currentUrl = getCurrentUrl();
+    if (this.lastViewedPage === currentUrl) {
+      return;
+    }
+    this.lastViewedPage = currentUrl;
 
-    const headers = getBasicEventHeaders();
-
-    this.sendRequest('/p', payload, headers);
+    this.trackEvent('$$pageView');
   }
 
-  trackEvent(name: string, customData?: Record<string, unknown>) {
+  trackEvent(name: string, customData?: Record<string, unknown>, beacon?: boolean) {
     this.checkInitialized();
 
-    const payload = {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const payload: any = {
       ...getBasicEventData(),
       name,
-      customData,
     };
+    if (customData) {
+      payload.customData = customData;
+    }
 
-    const headers = getBasicEventHeaders();
-
-    this.sendRequest('/e', payload, headers);
+    if (beacon) {
+      const headers = {
+        type: 'application/json',
+      };
+      const blob = new Blob([JSON.stringify(payload)], headers);
+      navigator.sendBeacon(`${this.options.url}/e`, blob);
+    } else {
+      const headers = getBasicEventHeaders();
+      this.sendRequest('/e', payload, headers);
+    }
   }
 
   resetUser() {
     this.checkInitialized();
     this.identifier = undefined;
 
-    // TODO: implement on backend side
+    // TODO: implement on backend side .. would be nice to just delete the cookie on the frontend though?
     this.sendRequest('/r');
   }
 
@@ -104,6 +139,40 @@ class Vemetric {
 
     this.sendRequest('/i', payload);
     // TODO: error handling, reset identifier
+  }
+
+  enableTrackPageViews() {
+    const pageView = () => this.trackPageView();
+
+    const originalPushState = window.history.pushState;
+    if (originalPushState) {
+      window.history.pushState = function (...args) {
+        originalPushState.apply(this, args);
+        pageView();
+      };
+      window.addEventListener('popstate', pageView);
+    }
+
+    window.addEventListener('hashchange', pageView);
+  }
+
+  enableTrackOutboundLinks() {
+    document.addEventListener('click', (event) => {
+      const target = event.target as HTMLElement;
+      if (target.tagName !== 'A') {
+        return;
+      }
+
+      const href = target.getAttribute('href');
+      if (!href) {
+        return;
+      }
+
+      const url = new URL(href, getCurrentUrl());
+      if (url.origin !== window.location.origin) {
+        this.trackEvent('$$outboundLink', { href }, true);
+      }
+    });
   }
 }
 
